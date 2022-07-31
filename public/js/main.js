b9697e93ba819a71cece0bcd45feb1c660291f01
Kris Marshall <rflctnofu@hotmail.com>
2022-07-31 15:01:31 -0400

    got basic seed set up, basic js file for main.handlebars, basic models set up

---
+++ public/js/main.js
@@ -0,0 +1,17 @@
+const home = document.getElementById('home');
+const dashboard = document.getElementById('dashboard');
+const login = document.getElementById('login');
+const logout = document.getElementById('logout');
+
+home.addEventListener('click', () => {
+    // console.log('HOME');
+});
+dashboard.addEventListener('click', () => {
+    // console.log('DASHBOARD');
+});
+login.addEventListener('click', () => {
+    // console.log('LOGIN');
+});
+logout.addEventListener('click', () => {
+    // console.log('LOGOUT');
+});
