49569928387f777e5d1340c1529dc9cc2b06730d
Kris Marshall <rflctnofu@hotmail.com>
2022-07-31 14:31:49 -0400

    got landing page to display, and server running

--- server.js
+++ server.js
@@ -32,8 +32,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use(routes);
+app.use(routes);
 
-// sequelize.sync({ force: false }).then(() => {
-app.listen(PORT, () => console.log('Now listening'));
-// });  
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});  
