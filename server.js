20bd25c6d30aca034c50b3c76ad779a58eaf21ef
Kris Marshall <rflctnofu@hotmail.com>
2022-07-30 21:04:40 -0400

    started writing main

--- server.js
+++ server.js
@@ -18,9 +18,9 @@ const sess = {
     cookie: {},
     resave: false,
     saveUninitialized: true,
-    store: new SequelizeStore({
-        db: sequelize
-    })
+    // store: new SequelizeStore({
+    //     db: sequelize
+    // })
 };
 
 app.use(session(sess));
@@ -32,8 +32,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(routes);
+// app.use(routes);
 
-sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening'));
-});  
+// sequelize.sync({ force: false }).then(() => {
+app.listen(PORT, () => console.log('Now listening'));
+// });  
