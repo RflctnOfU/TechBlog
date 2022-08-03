8f2305f4cc5a24458745119e26c329e6af1228fb
Kris Marshall <rflctnofu@hotmail.com>
2022-08-03 17:22:03 -0400

    got confirmation that db can be updated...js code is not working

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -2,19 +2,29 @@ const router = require('express').Router();
 const withAuth = require('../../utils/auth');
 const { User, Comment, Posts } = require('../../models');
 
-router.post('/', withAuth, async (req, res) => {
+router.post('/', async (req, res) => {
     try {
         const postData = await Posts.create({
             title: req.body.title,
             text: req.body.text,
             user_id: req.session.user_id,
         });
-        res.status(200).json(postData)
+        res.status(200).json(postData);
+        res.render('dashboard');
     } catch (err) {
         res.status(500).json(err);
     }
-    res.render('dashboard');
+
 });
+
+router.get('/', async (req, res) => {
+    try {
+        const postsData = await Posts.findAll();
+        res.json(postsData);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+})
 
 
 module.exports = router;
