55c1b3966155621d3b3b173b242667b28bc5caf8
Kris Marshall <rflctnofu@hotmail.com>
2022-08-03 15:59:59 -0400

    trying to get the post new post to work

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,5 +1,20 @@
 const router = require('express').Router();
+const withAuth = require('../../utils/auth');
+const { User, Comment, Posts } = require('../../models');
 
+router.post('/', withAuth, async (req, res) => {
+    try {
+        const postData = await Posts.create({
+            title: req.body.title,
+            text: req.body.text,
+            user_id: req.session.user_id,
+        });
+        res.status(200).json(postData)
+    } catch (err) {
+        res.status(500).json(err);
+    }
+    res.render('dashboard');
+});
 
 
 module.exports = router;
--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -39,3 +39,27 @@ const newPost = async () => {
 };
 
 document.querySelector('#new-post').addEventListener('click', newPost);
+
+const createPostHandler = async (e) => {
+    e.preventDefault();
+
+    const title = document.querySelector('#post-title').value;
+    const text = document.querySelector('#post-text').value;
+
+    if (title && text) {
+        const response = await fetch('/api/posts', {
+            method: 'POST',
+            body: JSON.stringify({ title, text }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            console.log('success');
+            document.location.replace('/');
+        } else {
+            alert('Failed to make new post.')
+        }
+    }
+}
+
+document.querySelector('#post').addEventListener('submit', createPostHandler);
