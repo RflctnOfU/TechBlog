35c65e35b7aa3769fd70cc2f8fa3322650c975f0
Kris Marshall <rflctnofu@hotmail.com>
2022-08-06 10:54:32 -0400

    added editcomment view, getting routes up and running

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Comment } = require('../../models');
+const { Comment, User } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 
@@ -27,5 +27,32 @@ router.get('/', async (req, res) => {
         res.status(500).json(err);
     }
 });
+
+router.get('/edit/:id', async (req, res) => {
+    try {
+        const commentData = await Comment.findByPk(req.params.id, {
+            where: {
+                id: req.params.id
+            },
+            attributes: ['id', 'text', 'user_id', 'posts_id'],
+            include: [
+                {
+                    model: User,
+                    attributes: ['id', 'name']
+                }
+            ]
+        });
+        const comment = commentData.get({ plain: true });
+        res.render('editComment', {
+            layout: 'main.handlebars',
+            comment,
+            logged_in: req.session.logged_in
+        })
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+router.put('/')
 
 module.exports = router;
